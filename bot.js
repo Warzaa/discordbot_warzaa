444784848e7f1522f8a7361e034414bdca5a69d8
Warzaa <34454605+Warzaa@users.noreply.github.com>
2018-01-04 19:35:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,15 +13,18 @@ client.on('message', message => {
 
 client.on('guildMemberAdd', member => {
     member.guild.channels.get('390028038688997379').send('**' + member.user.username + '**, Bienvenue dans la ParasiteSquad! Prepare toi a faire du sale :fire:'); 
-
-var role = member.guild.role.find('name', 'god')
-
-member.addRole(role)
-
-
 });
 
 
+client.on('guildMemberAdd', member => {
+  
+    
+console.log('User ' +  member.user.username + ' est venu')   
+    
+var role = member.guild.role.find('name', 'god')
+
+member.addRole(role)
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
