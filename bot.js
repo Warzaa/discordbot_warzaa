ec6531a168b9b9699a5dd4f9543f44307a35f003
Warzaa <34454605+Warzaa@users.noreply.github.com>
2018-01-04 18:31:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,11 +15,7 @@ client.on('guildMemberAdd', member => {
     member.guild.channels.get('390028038688997379').send('**' + member.user.username + '**, Bienvenue dans la ParasiteSquad! Prepare toi a faire du sale :fire:'); 
 });
 
-var role = member.guild.role.find('name', 'god')
-;
 
-member.addRole(role)
-;
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
